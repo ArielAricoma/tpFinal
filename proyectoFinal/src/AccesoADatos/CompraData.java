6b702f4a0e7c66023227a61319144571a23204b3
ArnaldoRom <142050350+ArnaldoRom@users.noreply.github.com>
2023-10-27 10:41:31 -0300

    clase dominio de compra realizada

--- proyectoFinal/src/AccesoADatos/CompraData.java
+++ proyectoFinal/src/AccesoADatos/CompraData.java
@@ -1,7 +1,18 @@
 
 package AccesoADatos;
 
+import java.sql.Connection;
+
 
 public class CompraData {
+    private Connection conexion;
+
+    public CompraData() {
+       conexion= Conexion.conectar();
+    }
+    
+    
+    
+    
 
 }
--- proyectoFinal/src/Dominio/Compra.java
+++ proyectoFinal/src/Dominio/Compra.java
@@ -1,7 +1,57 @@
 
 package Dominio;
 
+import java.time.LocalDate;
+
 
 public class Compra {
+    private int idCompra;
+    private Proveedor provedor;
+    private LocalDate fecha;
+
+    public Compra() {
+    }
+
+    public Compra(Proveedor provedor, LocalDate fecha) {
+        this.provedor = provedor;
+        this.fecha = fecha;
+    }
+
+    public Compra(int idCompra, Proveedor provedor, LocalDate fecha) {
+        this.idCompra = idCompra;
+        this.provedor = provedor;
+        this.fecha = fecha;
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Proveedor getProvedor() {
+        return provedor;
+    }
+
+    public void setProvedor(Proveedor provedor) {
+        this.provedor = provedor;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" + "idCompra=" + idCompra + ", provedor=" + provedor + ", fecha=" + fecha + '}';
+    }
+    
+    
 
 }
