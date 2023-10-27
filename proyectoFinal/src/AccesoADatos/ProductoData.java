3d37d969063454e47483ef5999e7ba23f0c6509c
ArielAricoma <ariel16ary@gmail.com>
2023-10-27 19:37:01 -0300

    JuanjoPasion

--- proyectoFinal/src/AccesoADatos/ProductoData.java
+++ proyectoFinal/src/AccesoADatos/ProductoData.java
@@ -1,7 +1,35 @@
 
 package AccesoADatos;
 
+import Dominio.Producto;
+import java.sql.Connection;
+import java.util.List;
+
 
 public class ProductoData {
-
+    private Producto producto= null;
+    private Connection conexion = null;
+    
+    public ProductoData(){
+        conexion = Conexion.conectar();
+    }
+    
+    public void registroProducto(Producto producto){
+    
+    }
+    
+    public void modificarProducto(Producto producto){
+    
+    }
+    public void eliminarProducto(int idProducto){
+    
+    }
+    
+    public Producto consultaProductoPorID(int idProducto){
+    
+    }
+    
+    public List<Producto> listarProductos(){
+    
+    }
 }
--- proyectoFinal/src/Dominio/Producto.java
+++ proyectoFinal/src/Dominio/Producto.java
@@ -3,5 +3,69 @@ package Dominio;
 
 
 public class Producto {
+    private int idProducto;
+    private String nombre;
+    private String descripcion;
+    private int precio;
+    private int descuento;
+    private int estado;
 
+    public Producto(int idProducto, String nombre, String descripcion, int precio, int descuento, int estado) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.descuento = descuento;
+        this.estado = estado;
+    }
+    public Producto(){}
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
 }
