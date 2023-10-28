e62a88a95059d2eaca7797619abed78077b281cb
ArnaldoRom <142050350+ArnaldoRom@users.noreply.github.com>
2023-10-28 20:39:56 -0300

    se empesaron a implementar medotos a CompraData

--- proyectoFinal/src/AccesoADatos/CompraData.java
+++ proyectoFinal/src/AccesoADatos/CompraData.java
@@ -1,7 +1,20 @@
 
 package AccesoADatos;
 
+import Dominio.Compra;
+import Dominio.Proveedor;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 
 public class CompraData {
@@ -10,6 +23,100 @@ public class CompraData {
     public CompraData() {
        conexion= Conexion.conectar();
     }
+    
+    public void registrarCompra(Compra compra){
+        String sql="INSERT compra (idProveedor,fecha,estado) VALUES(?, ?, ?)";
+        try {
+            PreparedStatement añadir=conexion.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            añadir.setInt(1,compra.getProveedor().getIdProveedor());
+            añadir.setDate(2,Date.valueOf(compra.getFecha()));
+            añadir.setBoolean(3,compra.isEstado());
+            añadir.executeUpdate();
+            ResultSet añadido=añadir.getGeneratedKeys();
+            if(añadido.next()){
+                compra.setIdCompra(añadido.getInt("idCompra"));
+                JOptionPane.showMessageDialog(null,"Compra añadida");
+            }
+            añadir.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error Ingresar tabla compra");
+        }        
+    }
+    
+    public List<Proveedor> listarCompraporProveedor(int idProveedor){
+        String sql="SELECT * FROM proveedor WHERE idProverdor=? AND estado=1";
+        ArrayList<Proveedor> listado= new ArrayList<>();
+        
+        try {
+            PreparedStatement lista=conexion.prepareStatement(sql);
+            lista.setInt(1,idProveedor);
+            ResultSet rs=lista.executeQuery();
+            while(rs.next()){
+                int idProv=rs.getInt("idProveedor");
+                String nombre=rs.getString("razónSocial");
+                String domicilio=rs.getString("domicilio");
+                String telefono=rs.getString("telefono");
+                boolean estado=rs.getBoolean("estado");
+                Proveedor pro=new Proveedor(idProv,nombre,domicilio,telefono,estado);
+                listado.add(pro);
+            }
+            lista.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error con Listar Proveedor");
+        }
+        return listado;
+        
+    }
+    
+    public List<Compra> entreFechas(LocalDate inicio, LocalDate fin){
+        String sql="SELECT * FROM compra WHERE fecha between ? and ?";
+        ArrayList<Compra> compras=new ArrayList<>();
+        
+        try {
+            PreparedStatement lista=conexion.prepareStatement(sql);
+            //lista.setDate(1,inicio);
+            //lista.setDate(2, fin);
+            ResultSet rs=lista.executeQuery();
+            while(rs.next()){
+                
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error con Lista Compra");
+        }
+        
+        return compras;
+    }
+    
+    // sacar idProveedor
+    public Compra obtenerCompraPorID(int idCompra){
+        String sql="SELECT * FROM compra WHERE idCompra=? AND estado=1";
+        Compra compra=null;
+        
+        try {
+            PreparedStatement obtener= conexion.prepareStatement(sql);
+            obtener.setInt(1, idCompra);
+            ResultSet lista=obtener.executeQuery();
+            
+            while(lista.next()){
+                
+                int Proveedor=lista.getInt("idProveedor");
+                LocalDate fecha=lista.getDate("fecha").toLocalDate();
+                boolean estado=lista.getBoolean("estado");
+                
+               // compra=new Compra(idCompra,Proveedor,fecha,estado);
+            }
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null,"Error al Obtener Compras");
+        }
+        return compra;
+        
+    }
+    
+    //public List<Producto> listarCompraporProducto(){
+        
+    //}
+    
+    
     
     
     
