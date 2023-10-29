b9e905499c14677567446b0c85bdf69f8eaf222d
ArielAricoma <ariel16ary@gmail.com>
2023-10-28 21:29:54 -0300

    No duermo

--- proyectoFinal/src/AccesoADatos/ProductoData.java
+++ proyectoFinal/src/AccesoADatos/ProductoData.java
@@ -2,8 +2,10 @@
 package AccesoADatos;
 
 import Dominio.Producto;
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 
 public class ProductoData {
@@ -14,26 +16,91 @@ public class ProductoData {
         conexion = Conexion.conectar();
     }
     
-    public void registroProducto(Producto producto){
-    
+    public void registroProducto(Producto produ){
+        String sql="INSER INTO producto(nombre,descripcion,precio, descuento,estado) VALUES(?, ?, ?, ?, ?)";
+        producto= new Producto();
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            
+            ps.setString(1, produ.getNombre());
+            ps.setString(2, produ.getDescripcion());
+            ps.setDouble(3, produ.getPrecio());
+            ps.setDouble(4, produ.getDescuento());
+            ps.setBoolean(5, produ.isEstado());
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"producto incorrecto");
+        }
     }
     
-    public void modificarProducto(Producto producto){
-    
+    public void modificarProducto(Producto produ){
+        
+        String sql="UPDATE producto SET nombre = ?, descripcion=?, precio=?, descuento=?, estado=? WHERE idProducto=?";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setString(1, produ.getNombre());
+            ps.setString(2, produ.getDescripcion());
+            ps.setDouble(3, produ.getPrecio());
+            ps.setDouble(4, produ.getDescuento());
+            ps.setBoolean(5, true);
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"Producto no encontrado","error de conexion",0);
+        }
     }
     public void eliminarProducto(int idProducto){
-    
+        String sql="UPDATE producto SET estado = 0 WHERE idProducto = ?";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1,idProducto);
+            ps.executeUpdate();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"Producto no encontrado");
+        }
     }
     
     public Producto consultaProductoPorID(int idProducto){
-    
+        String sql="SELECT * FROM producto WHERE idProducto = ?";
+        Producto producto = null;
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, idProducto);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                producto =new Producto();
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setNombre(rs.getString("nombre"));
+                producto.setDescripcion("descripcion");
+                producto.setPrecio(rs.getDouble("precio"));
+                producto.setDescuento(rs.getDouble("descuento"));
+                producto.setEstado(rs.getBoolean("estado"));
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"Producto no encontrado");
+        }
+        
+        
         return null;
-    
+   
     }
     
     public List<Producto> listarProductos(){
-    
-        return null;
-    
+        String sql="SELECT * FROM producto WHERE estado = 1";
+        List<Producto>listaProducto = new ArrayList();
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Producto producto= new Producto();
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setNombre(rs.getString("nombre"));
+                producto.setDescripcion(rs.getString("descripcion"));
+                producto.setPrecio(rs.getDouble("precio"));
+                producto.setDescuento(rs.getDouble("descuento"));
+                producto.setEstado(rs.getBoolean("estado"));
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"No hay Productos");
+        }
+        return listaProducto;
     }
 }
