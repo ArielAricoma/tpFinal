e7ba26d510c0cbd03b0cf0f00d24e94db388d92b
matgithubulp <ejarquegithub@gmail.com>
2023-11-01 17:33:21 -0300

    metodosUsuario

--- proyectoFinal/src/AccesoADatos/UsuarioData.java
+++ proyectoFinal/src/AccesoADatos/UsuarioData.java
@@ -2,26 +2,51 @@
 package AccesoADatos;
 
 import Dominio.Usuario;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class UsuarioData {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public void buscarUsuario(){}
-    
-    public void recuperarUsuario(){
-    
+    private Connection conexion = null;
+
+    public UsuarioData(){
+        conexion = Conexion.conectar();
     }
+    
+    
+   
+    
+//idUsuario, nombreCuenta, contrasena, correoElec, estado
+    public Usuario buscarUsuario(String nombre){
+        String sql = "SELEECT * FROM usuario WHERE nombreUsuario = ?"; //buscar en la teoria como se hacia para buscar por contenido o comienzo 
+        Usuario user = null;
+        try {
+            
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setString(1, nombre);
+            
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                user = new Usuario();
+                user.setIdUsuario(rs.getInt("idUsuario"));
+                user.setNombreCuenta(rs.getString("nombreUsuario"));
+                user.setContrasena(rs.getString("contrasena"));
+                user.setCorreoElec(rs.getString("correoElec"));
+                rs.close();
+                ps.close();
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return user;
+    }
+    
+    public void listarUsuarios(){}
     
     public void crearUsuario(Usuario user){
 
