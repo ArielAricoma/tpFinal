a2ca96f078608c79ac7e3ea0ec0d55150dca62fa
matgithubulp <ejarquegithub@gmail.com>
2023-10-31 23:13:33 -0300

    AddToUsuario

---
+++ proyectoFinal/src/AccesoADatos/UsuarioData.java
@@ -0,0 +1,49 @@
+
+package AccesoADatos;
+
+import Dominio.Usuario;
+
+
+public class UsuarioData {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void buscarUsuario(){}
+    
+    public void recuperarUsuario(){
+    
+    }
+    
+    public void crearUsuario(Usuario user){
+
+    
+    }
+    
+    public void cambiarContasena(){
+    
+    }
+    
+    public void darDeBaja(){}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
